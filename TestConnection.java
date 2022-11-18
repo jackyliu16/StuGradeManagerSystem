87933fc92f09b2285a1fcec68b398a010613585d
jackyliu <18922251299@163.com>
2022-11-18 11:53:26 +0800

    + update(TestConnection, ResultSetPrinter):
    	1. ResultSetPrinter: 	Adding a ResultSetPrinter for debug reference by
    	   https://blog.csdn.net/qq_21808961/article/details/102712411
    and add a little change for output format
    	2. (Jar):		Adding a jar Package for Connection to MySQL in ubuntu
    	3. TestConnection: 	Making a simple Connection - Run SQL Example
    	修改：     CourseDB.sql
    	新文件：   TestConnection.java
    	新文件：   Tool/ResultSetPrinter.java
    	新文件：   jar/mysql-connector-j_8.0.31-1ubuntu22.04_all.deb

---
+++ TestConnection.java
@@ -0,0 +1,59 @@
+/*
+ * @File:   TestConnection.java
+ * @Desc:
+ * @Author: jacky
+ * @Date:   2022/11/18 上午10:53
+ * @Version:0.0
+ */
+
+import Tool.ResultSetPrinter;
+
+import java.sql.*;
+
+public class TestConnection {
+
+//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL      = "jdbc:mysql://localhost:3306/CourseDB";
+    static final String USER        = "root";
+    static final String PWD         = "123456";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt  = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connect to DataBases");
+            conn = DriverManager.getConnection(DB_URL, USER, PWD);
+
+            stmt = conn.createStatement();
+
+            String sql = "select * from Student";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            ResultSetPrinter.printResultSet(rs);
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se) {
+                // do nothing
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        System.out.println("END");
+    }
+}
