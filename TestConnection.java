5aa58eed0e8c44f8634e6f6fc4c0275e6cb74b6f
jackyliu <18922251299@163.com>
2022-11-19 18:22:45 +0800

    format

--- TestConnection.java
+++ TestConnection.java
@@ -13,57 +13,45 @@ import java.util.ArrayList;
 
 public class TestConnection {
 
-//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    // static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL      = "jdbc:mysql://localhost:3306/CourseDB";
-    static final String USER        = "root";
-    static final String PWD         = "123456";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/CourseDB";
+    static final String USER = "root";
+    static final String PWD = "123456";
 
     public static void main(String[] args) {
         Connection conn = null;
-        Statement stmt  = null;
+        Statement stmt = null;
 
         try {
             Class.forName(JDBC_DRIVER);
             System.out.println("Connect to DataBases");
             conn = DriverManager.getConnection(DB_URL, USER, PWD);
-            stmt = conn.createStatement();//statement是sql语句？connnet创建的
+            stmt = conn.createStatement();// statement是sql语句？connnet创建的
 
-            String sql = "select * from teacher";//瞎玩的
+            String sql = "select * from teacher";
             ResultSet rs = stmt.executeQuery(sql);
-            ResultSetOperation.printResultSet(rs);      // using for debug
+            ResultSetOperation.printResultSet(rs); // using for debug
             ArrayList<ArrayList<String>> res = ResultSetOperation.convertResultSetIntoArrayList(rs); // using for output
-            System.out.println(res);//怎么样把多个查询的结果输出？
+            System.out.println(res);// 怎么样把多个查询的结果输出？
             rs.close();
             stmt.close();
             conn.close();
-        }
-        catch (SQLException se)
-        {
+        } catch (SQLException se) {
             se.printStackTrace();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
                 if (stmt != null)
                     stmt.close();
-            }
-            catch (SQLException se)
-            {
+            } catch (SQLException se) {
                 // do nothing
             }
-            try
-            {
+            try {
                 if (conn != null)
                     conn.close();
-            }
-            catch (SQLException se)
-            {
+            } catch (SQLException se) {
                 se.printStackTrace();
             }
         }
