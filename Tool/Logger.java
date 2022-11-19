12fa1e7c7a4b4c3626d4dd53d22c1418cd20060c
jackyliu <18922251299@163.com>
2022-11-19 17:00:20 +0800

    remove out and .idea

---
+++ Tool/Logger.java
@@ -0,0 +1,12 @@
+package Tool;
+/*
+ * @File:   Logger.java
+ * @Desc:
+ * @Author: jacky
+ * @Repo:   https://github.com/jackyliu16
+ * @Date:   2022/11/19 下午4:49
+ * @Version:0.0
+*/
+
+public class Logger {
+}
