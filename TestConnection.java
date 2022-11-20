3ec3d43ac56ff1d7c2c1823aed1c67f055a681c9
Jack5057 <2115634142@qq.com>
2022-11-20 11:26:31 +0800

    新增switch语句，支持插入和删除

--- TestConnection.java
+++ TestConnection.java
@@ -1,59 +1,80 @@
-/*
- * @File:   TestConnection.java
- * @Desc:
- * @Author: jacky
- * @Date:   2022/11/18 上午10:53
- * @Version:0.0
- */
-
-import Tool.Logger;
 import Tool.ResultSetOperation;
 import java.util.*;
 import java.sql.*;
 import java.util.ArrayList;
 public class TestConnection {
-
-//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/CourseDB";
-    static final String USER = "root";
-    static final String PWD = "123456";
-    static final Logger log = new Logger();
-
+    static final String DB_URL      = "jdbc:mysql://localhost:3306/CourseDB";
+    static final String USER        = "root";
+    static final String PWD         = "123456";
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt  = null;
-
+        Scanner sc=new Scanner(System.in);
         try {
             Class.forName(JDBC_DRIVER);
-            System.out.println("Connect to DataBases");
+            System.out.println("Successfully connected to dataBase");
             conn = DriverManager.getConnection(DB_URL, USER, PWD);
-            stmt = conn.createStatement();//statement是sql语句？connnet创建的
-
-            String sql = "select * from teacher";//瞎玩的
-            ResultSet rs = stmt.executeQuery(sql);
-            ResultSetOperation.printResultSet(rs);      // using for debug
-            ArrayList<ArrayList<String>> res = ResultSetOperation.convertResultSetIntoArrayList(rs); // using for output
-            System.out.println(res);//怎么样把多个查询的结果输出？
-            rs.close();
+            stmt = conn.createStatement();
+            System.out.println("Choosefrom:query,insert,delete");
+            String op=sc.nextLine();
+            switch(op)
+            {
+            case "query"://查询操作
+                System.out.println("Enter your query here:");
+                String sqlQuery =sc.nextLine();
+                ResultSet rs = stmt.executeQuery(sqlQuery);
+                ResultSetOperation.printResultSet(rs);      // debug用的
+                ArrayList<ArrayList<String>> res = ResultSetOperation.convertResultSetIntoArrayList(rs); //你们指定的输出格式
+                System.out.println(res);
+            break;
+            
+            case"insert"://插入操作
+                System.out.println("Enter your insert:");
+                String sqlInsert=sc.nextLine();
+                stmt.executeUpdate(sqlInsert);
+                System.out.println("Success");
+            break;
+            
+            case"delete"://删除操作
+            System.out.println("Enter your delete:");
+                String sqlDelete=sc.nextLine();
+                stmt.executeUpdate(sqlDelete);
+                System.out.println("Success");
+            break;
+            default:
+            System.out.println("Unknown command!");
+            }
+            //rs.close();
             stmt.close();
             conn.close();
-            log.info("sql connection success !");
-        } catch (Exception se) {
+        }
+        catch (SQLException se)
+        {
             se.printStackTrace();
-        } finally {
-            try {
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
                 if (stmt != null)
                     stmt.close();
             }
             catch (SQLException se)
             {
-                // do nothing
+                System.out.println("Failed to connect to DB!");
             }
-            try {
+            try
+            {
                 if (conn != null)
                     conn.close();
-            } catch (SQLException se) {
+            }
+            catch (SQLException se)
+            {
                 se.printStackTrace();
             }
         }
