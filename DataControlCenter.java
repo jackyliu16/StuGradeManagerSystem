c1b522863134ca78073a853dd7c97fb69b4ba86e
jackyliu <18922251299@163.com>
2022-11-20 13:59:45 +0800

    update(DataControlCenter.java):
    1. Adding function(checkIfStudentInCourse, checkIfStudentInExCourse, insertStudentIntoExCourse, updateStudentExCourseGrade)
    
    2. docs

--- DataControlCenter.java
+++ DataControlCenter.java
@@ -37,28 +37,149 @@ public class DataControlCenter {
         }
     }
 
+    /**
+     * Gain all grade for all courses taken by the student
+     *
+     * @param student_id student ID
+     * @return Array<Array < String>> for a table which show this
+     */
     public ArrayList<ArrayList<String>> getStudentCourseGrade(String student_id) {
         ArrayList<ArrayList<String>> res = new ArrayList<>();
         try (Statement stmt = conn.createStatement()) {
             String sql = String.format(
                     "SELECT Student.StuNo, Student.StuName, Learn.Grade " +
                             "FROM Student, ExClass, Learn " +
-                            "WHERE Student.StuNo = Learn.StuNo AND Learn.ExClassNo = ExClass.ExClassNo AND Student.StuNo = '%s';", student_id);
+                            "WHERE Student.StuNo = Learn.StuNo AND Learn.ExClassNo = ExClass.ExClassNo AND Student.StuNo = '%s';",
+                    student_id);
             log.info(String.format("sql: %s", sql));
             ResultSet rs = stmt.executeQuery(sql);
-//            ResultSetOperation.printResultSet(rs);
             rs.close();
             stmt.close();
             log.info("query success!");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        // do nothing
         return res;
     }
 
+    /**
+     * Check if student In Course
+     *
+     * @param student_id Student ID
+     * @param course_id  Course ID
+     */
+    public Boolean checkIfStudentInCourse(String student_id, String course_id) {
+        boolean flag = false;
+        try (Statement stmt = conn.createStatement()) {
+            String sql = String.format(
+                    "SELECT * " +
+                            "FROM Course, ExClass, Learn " +
+                            "WHERE Course.CourseNo = ExClass.CourseNo AND ExClass.ExClassNo = Learn.ExClassNo " +
+                            "AND Learn.StuNo = \"%s\" AND Course.CourseNo = \"%s\";", student_id, course_id);
+            log.debug(String.format("sql: %s", sql));
+            ResultSet rs = stmt.executeQuery(sql);
+//            ResultSetOperation.printResultSet(rs);    // 不允许添加输出，这样会使迭代器运行到结尾
+            if (rs.next()) {
+                flag = true;
+            }
+            log.debug(String.format("flag is: %s", flag));
+            rs.close();
+            stmt.close();
+            log.info("query success!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return flag;
+    }
+
+    /**
+     * check if student in the ExCourse
+     *
+     * @param student_id   student ID
+     * @param ex_course_id The ExCourseNo
+     * @return true / false
+     */
+    public Boolean checkIfStudentInExCourse(String student_id, String ex_course_id) {
+        boolean flag = false;
+        try (Statement stmt = conn.createStatement()) {
+            String sql = String.format("" +
+                    "SELECT * " +
+                    "FROM Learn " +
+                    "WHERE Learn.StuNo = '%s' AND Learn.ExClassNo = '%s';", student_id, ex_course_id);
+            log.debug(String.format("sql: %s", sql));
+            ResultSet rs = stmt.executeQuery(sql);
+//            ResultSetOperation.printResultSet(rs);    // 不允许添加输出，这样会使迭代器运行到结尾
+            if (rs.next()) {
+                flag = true;
+            }
+            log.debug(String.format("flag is: %s", flag));
+//            rs.close();
+            stmt.close();
+            log.info("query success!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return flag;
+    }
+
+    /**
+     * Adding a student into a ExCourse
+     *
+     * @param student_id  Student ID
+     * @param ExCourse_id ExCourse ID that Student will be inserted into
+     */
+    public Boolean insertStudentIntoExCourse(String student_id, String ExCourse_id) {
+        try (Statement stmt = conn.createStatement()) {
+            String sql = String.format("insert into Learn values ('%s', '%s', null);", student_id, ExCourse_id);
+            log.debug(String.format("sql: %s", sql));
+            int rs = stmt.executeUpdate(sql);
+            log.trace(String.format("rs: %s", rs));
+            stmt.close();
+            log.info("query success!");
+            return rs == 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * update a student grade in a ExCourse
+     * anno: caller need to make sure the student is in the course before run this function
+     *
+     * @param student_id  studentNo
+     * @param ExCourse_id The ExClassNo that student in
+     * @param grade       double grade
+     * @return true -> Succeed; false -> failure
+     */
+    public Boolean updateStudentExCourseGrade(String student_id, String ExCourse_id, double grade) {
+        // if student is not in the class
+        if (!checkIfStudentInExCourse(student_id, ExCourse_id)) {
+            log.error(String.format("student %s not found in the course %s", student_id, ExCourse_id));
+            return false;
+        }
+        try (Statement stmt = conn.createStatement()) {
+            String sql = String.format("" +
+                    "UPDATE Learn " +
+                    "SET Grade='%s' WHERE StuNo='%s' AND ExClassNo = '%s';", grade, student_id, ExCourse_id);
+            log.info(String.format("sql: %s", sql));
+            int rs = stmt.executeUpdate(sql);
+            stmt.close();
+            if (rs == 1) {
+                log.info("update query success!");
+                return true;
+            } else {
+                log.error(String.format("update query failure: %s", sql));
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     public static void main(String[] args) {
-        DataControlCenter dcc  = new DataControlCenter();
-        dcc.getStudentCourseGrade("20200740001");
+        DataControlCenter dcc = new DataControlCenter();
+//        System.out.println(dcc.checkIfStudentInCourse("20200740001", "00000001"));
+        dcc.insertStudentIntoExCourse("20200740002", "00000004");
     }
 }
